81cb9f72c6dbbc90a9e8e7517ed6570bcd4a46af
denis <ts-alan@mail.ru>
2015-06-18 16:14:10 +0300

    Билборды. Добавление объекта по координатам

--- Adbase/Scripts/AddPlacamerkToMap.js
+++ Adbase/Scripts/AddPlacamerkToMap.js
@@ -1,11 +1,39 @@
-﻿$(function () {
-    ymaps.ready(init);
+﻿
+    
     var myMap;
     var myPlacemark;
     var havePlacemark = false;
+    ymaps.ready(init);
+
+    //присвоение коодинат при перетаскивание метки 
+    function dragend() {
+
+        myPlacemark.events.add('dragend', function (e) {
+
+            var coords = myPlacemark.geometry.getCoordinates();
+            console.log(coords);
+            var str = String(coords[0].toPrecision(6));
+            str = str.replace('.', ',');
+            //if (!isNaN(str)) {
+            //    $("#Shirota").val(str);
+            //} else {
+            //    $("#Shirota").val("");
+            //}
+            $("#Shirota").val(str);
+            str = String(coords[1].toPrecision(6));
+            str = str.replace('.', ',');
+            //if (!isNaN(str)) {
+            //    $("#Dolgota").val(str);
+            //} else {
+            //    $("#Dolgota").val("");
+            //} 
+            $("#Dolgota").val(str);
+        });
+    }
+
     function init() {
         myMap = new ymaps.Map("map", {
-            center: [53.9172, 27.5601], 
+            center: [53.9172, 27.5601],
             zoom: 11,
             behaviors: ['default', 'scrollZoom']
         }, {
@@ -16,18 +44,21 @@
         myMap.controls.add('searchControl');
         myMap.controls.add('typeSelector');
 
-            //присвоение  коодинат при клике  
-            myMap.events.add('click', function (e) {
+
+
+        //присвоение  коодинат при клике  
+        myMap.events.add('click', function(e) {
             if (!havePlacemark) {
                 var coords = e.get('coordPosition');
                 myPlacemark = new ymaps.Placemark([coords[0].toPrecision(6), coords[1].toPrecision(6)],
-                    {},
-                    {
-                        draggable: true
-                    });
+                {},
+                {
+                    draggable: true
+                });
                 myMap.geoObjects.add(myPlacemark);
                 havePlacemark = true;
             }
+
 
             var str = String(coords[0].toPrecision(6));
             str = str.replace('.', ',');
@@ -35,58 +66,31 @@
             str = String(coords[1].toPrecision(6));
             str = str.replace('.', ',');
             $("#Dolgota").val(str);
-
-            //присвоение коодинат при перетаскивание метки  
-            myPlacemark.events.add('dragend', function (e) {
-                var coords = myPlacemark.geometry.getCoordinates();
-                var str = String(coords[0].toPrecision(6));
-                str = str.replace('.', ',');
-                //if (!isNaN(str)) {
-                //    $("#Shirota").val(str);
-                //} else {
-                //    $("#Shirota").val("");
-                //}
-                $("#Shirota").val(str);
-                str = String(coords[1].toPrecision(6));
-                str = str.replace('.', ',');
-                //if (!isNaN(str)) {
-                //    $("#Dolgota").val(str);
-                //} else {
-                //    $("#Dolgota").val("");
-                //} 
-                $("#Dolgota").val(str);
-            });
-
-
-                //присвоение координат при задание координат в инпутах
-            $("#Shirota").change(function () {
-                    SetCoordinates();
-                }
-              );
             
-            $("#Dolgota").change(function () {
-                    SetCoordinates();
-                }
-             );
-            function SetCoordinates() {
-                console.log(myPlacemark);
-                //console.log(myPlacemark.geometry._ec[0]);
-                //console.log(myPlacemark.geometry._ec[1]);
-                myMap.geoObjects.remove(myPlacemark);
-                //console.log($("#Shirota").val());
-                //console.log($("#Dolgota").val());
-                if ($("#Shirota").val() != undefined && $("#Dolgota").val() != undefined) {
-                    myPlacemark = new ymaps.Placemark([$("#Shirota").val(), $("#Dolgota").val()],
-                    {},
-                    {
-                        draggable: true
-                    });
-                    console.log(myPlacemark);
-                    //console.log(myPlacemark.geometry._ec[0]);
-                    //console.log(myPlacemark.geometry._ec[1]);
-                    myMap.geoObjects.add(myPlacemark);
-                }
-            }
+            dragend();
         });
+
+        //присвоение координат при задание координат в инпутах
+
     }
-});
+
+    function SetCoordinates() {
+
+        if ($("#Shirota").val() != "" && $("#Dolgota").val() != "") {
+            //удалить обьект
+            if (myPlacemark != undefined) {
+
+                myMap.geoObjects.remove(myPlacemark);
+            }
+            //присвоить обьект
+            myPlacemark = new ymaps.Placemark([$("#Shirota").val().replace(',', '.'), $("#Dolgota").val().replace(',', '.')],
+            {},
+            {
+                draggable: true
+            });
+        myMap.geoObjects.add(myPlacemark);
+            
+        }
+        dragend();
+    }
+
