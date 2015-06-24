d1365e3fd4ab483fb3c1a653cb92238c256a6505
denis <ts-alan@mail.ru>
2015-06-24 18:44:07 +0300

    Билборды (Добавление не до конца)

--- Adbase/Models/TableAdapterExtensions.cs
+++ Adbase/Models/TableAdapterExtensions.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 
 namespace Sciencecom.Models
 {
+    //public static int i;
     public static class TableAdapterExtensions
     {
-
-        //public static int Increment(bool reset)
+        
+        //public static int Increment()
         //{
-        //   TempData[]
-           
-        //} 
+        //    //Cache["increment"]=
+        //}
+
     }
+
+    public static class CustomHtmlHelpers
+    {
+        public static MvcHtmlString Side(this HtmlHelper html,int idSide)
+        {
+            return new MvcHtmlString("<div class=\"form-horizontal\" code>" +
+                                        "<div class=\"form-group\">"+
+                                        "<label class=\"control-label col-md-2\" for=\"\" style=\"margin-left: 55px\">Сторона"+idSide+" </label>"+
+                                        "</div>"+
+                                            "<div class=\"form-group\">"+
+                                                "<div class=\"col-md-10\">"+
+                                                   "<a href=\"#\" id=\"AddButton" + idSide + "\" style=\"color: #428bca; font-weight: bold;\">+Добавить площадь</a>" +
+                                                "</div>"+
+                                        "</div>"+
+                                        "<div id=\"InsertPartial" + idSide + "\"></div>" +
+                                    "</div>");
+        }
+    }
+
 }
