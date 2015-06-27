058c5776dc58e770271d87bd106913fd1335b03a
Denis <Ts-alan@mail.ru>
2015-06-27 21:29:47 +0200

    Подтянута Ef

--- Adbase/Models/Surface.cs
+++ Adbase/Models/Surface.cs
@@ -14,15 +14,15 @@ namespace Sciencecom.Models
     
     public partial class Surface
     {
-        public int Height { get; set; }
-        public int Width { get; set; }
-        public int Space { get; set; }
+        public Nullable<int> Height { get; set; }
+        public Nullable<int> Width { get; set; }
+        public Nullable<int> Space { get; set; }
         public string TypeOfAdvertising { get; set; }
         public string Story { get; set; }
         public Nullable<System.DateTime> StartDate { get; set; }
         public Nullable<System.DateTime> EndDate { get; set; }
         public int Id { get; set; }
-        public System.Guid Side_Id { get; set; }
+        public Nullable<System.Guid> Side_Id { get; set; }
         public Nullable<System.Guid> Topic { get; set; }
         public Nullable<System.Guid> SubTopic { get; set; }
         public string Identification { get; set; }
