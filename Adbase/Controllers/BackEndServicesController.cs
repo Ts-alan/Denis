25e03c89c7c4e3e4f639ff89acf0f9758ad20725
denis <ts-alan@mail.ru>
2015-07-29 19:14:29 +0200

    Поиск не до конца

--- Adbase/Controllers/BackEndServicesController.cs
+++ Adbase/Controllers/BackEndServicesController.cs
@@ -12,25 +12,31 @@ namespace Sciencecom.Controllers
     public class BackEndServicesController : Controller
     {
         
-        [Authorize]
-        public string GetSubTopic(Guid id)
-        {
+        //[Authorize]
+        //public string GetSubTopic(Guid id)
+        //{
          
-            using (SciencecomEntities _ctx =new SciencecomEntities())
-            {
-                var Subtopic = _ctx.RelationshipOfAdvertisings.Where(a => a.idOfTopicForSocialAdvertising == id).Select(a=>a.idOfSubtopicForSocialAdvertising).ToList();
-                IEnumerable GuidSubtopic = null;
-                foreach (var i in Subtopic)
-                {
-                    GuidSubtopic = _ctx.SubtopicForSocialAdvertisings.AsEnumerable().Where(a => Subtopic.Contains(a.id)).Select(a=>new {a.id,a.Name});   
-                }
+        //    using (SciencecomEntities _ctx =new SciencecomEntities())
+        //    {
+        //        var Subtopic = _ctx.RelationshipOfAdvertisings.Where(a => a.idOfTopicForSocialAdvertising == id).Select(a=>a.idOfSubtopicForSocialAdvertising).ToList();
+        //        IEnumerable GuidSubtopic = null;
+        //        foreach (var i in Subtopic)
+        //        {
+        //            GuidSubtopic = _ctx.SubtopicForSocialAdvertisings.AsEnumerable().Where(a => Subtopic.Contains(a.id)).Select(a=>new {a.id,a.Name});   
+        //        }
 
-                var result = JsonConvert.SerializeObject(GuidSubtopic, Formatting.Indented, new JsonSerializerSettings
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-                });
-                return result;
-            }
+        //        var result = JsonConvert.SerializeObject(GuidSubtopic, Formatting.Indented, new JsonSerializerSettings
+        //        {
+        //            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        //        });
+        //        return result;
+        //    }
+        //}
+        [HttpPost]
+        public void SaveSearch(string collectionMap)
+        {
+            Session["TT"] = collectionMap;
+            int i = 0;
         }
     }
 }
