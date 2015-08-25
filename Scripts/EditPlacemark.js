e9c8fb539f72b8c980fc079c09cf58bbdf2d9e28
denis <ts-alan@mail.ru>
2015-08-25 17:45:00 +0300

    Подпарвлен баг и добавлены изображения

--- Scripts/EditPlacemark.js
+++ Scripts/EditPlacemark.js
@@ -1,10 +1,11 @@
 ﻿function EditPlacemark(shirota, dolgota) {
-    ymaps.ready(init);
     var myMap;
     var myPlacemark;
     var havePlacemark = false;
-    shirota = parseFloat(shirota.replace(",", "."));
-    dolgota = parseFloat(dolgota.replace(",", "."));
+    var shirotaValue = parseFloat(shirota.replace(",", "."));
+    var dolgotaValue = parseFloat(dolgota.replace(",", "."));
+    ymaps.ready(init);
+
     function init() {
         myMap = new ymaps.Map("map", {
             center: [53.9172, 27.5601],
@@ -17,13 +18,16 @@
         myMap.controls.add('zoomControl');
         myMap.controls.add('searchControl');
         myMap.controls.add('typeSelector');
-        var myPlacemark = new ymaps.Placemark([shirota, dolgota],
+
+        var myPlacemark = new ymaps.Placemark([shirotaValue, dolgotaValue],
             {},
             {
                 draggable: true
             });
+        myMap.geoObjects.add(myPlacemark);
         myPlacemark.events.add('dragend', function (e) {
             var coords = myPlacemark.geometry.getCoordinates();
+            console.log(coords);
             var str = String(coords[0].toPrecision(6));
             str = str.replace('.', ',');
             $("#Shirota").val(str);
@@ -33,6 +37,6 @@
             $("#Dolgota").val(str);
         
         });
-        myMap.geoObjects.add(myPlacemark);
+       
     }
 }
