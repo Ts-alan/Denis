cf04baf59e885c1aa875d63d23590fd9cc9ed2d9
denis <ts-alan@mail.ru>
2015-09-02 17:49:00 +0300

    Создал базу данных,подогнал под все экраны,начал наносить разметку

--- OddBasyBY/OddBasyBY/Controllers/HomeController.cs
+++ OddBasyBY/OddBasyBY/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OddBasyBY.Models;
 
 namespace OddBasyBY.Controllers
 {
@@ -10,9 +11,18 @@ namespace OddBasyBY.Controllers
     {
         public ActionResult Index()
         {
+            using (OddAdbaseEntity db = new OddAdbaseEntity())
+            {
+                ViewBag.City = db.City.First().Name;
+            }
+
             return View();
         }
 
-   
+        public ActionResult SaveSuccess()
+        {
+            return View();
+        }
+
     }
 }
