0b4dce9e00d6b003b29664a0acda018961eab9e4
denis <ts-alan@mail.ru>
2015-09-24 13:07:36 +0300

    СОхранения,перезапись не до конца

--- OddBasyBY/OddBasyBY/Controllers/HomeController.cs
+++ OddBasyBY/OddBasyBY/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace OddBasyBY.Controllers
 
         public void SaveSuccess(City city, Street street , [ModelBinder(typeof(CustomModelBinderForSegment))] ICollection<Segment> segment,[ModelBinder(typeof(CustomModelBinderForModels))]ICollection<SpecificationofRM> models)
         {
+            if (db.Street.Any(a=>a.BreadthE==street.BreadthE&&a.BreadthS==street.BreadthS&&a.LengthE==street.LengthE&&a.LengthS==street.LengthS))
+            {
+                
+            }
             var streetInfo = new Street()
             {
                 Name = street.Name,
@@ -33,7 +37,7 @@ namespace OddBasyBY.Controllers
                 City_id = city.id
             };
             db.Street.Add(streetInfo);
-
+           
             streetInfo.Segment = segment;
             streetInfo.SpecificationofRM = models;
 
