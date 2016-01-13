81f8ffd30755641e054bce434c883e084e9bd7d1
Pu4 <Pu4k0ff2012@gmail.com>
2016-01-13 18:02:51 +0300

    Добавил в бд "Описание рамещаемой информации", поправил вьюшки по замечаниям.

--- Adbase/Models/AdvertisingStructure.cs
+++ Adbase/Models/AdvertisingStructure.cs
@@ -50,6 +50,7 @@ namespace Sciencecom.Models
         public Nullable<System.Guid> ViewBad_Id { get; set; }
         public string Support_ { get; set; }
         public Nullable<System.DateTime> CoordinationDate { get; set; }
+        public string ContentInformationDescription { get; set; }
     
         public virtual Backlight Backlight { get; set; }
         public virtual Locality Locality { get; set; }
