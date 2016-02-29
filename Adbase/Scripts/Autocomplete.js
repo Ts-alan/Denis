1362ee1fb971f14824ea81b024d8fc161c6fc5df
Pu4 <Pu4k0ff2012@gmail.com>
2016-02-29 17:26:51 +0300

    Допилiл автокомплит, взлетела валидация, облагорожено создание световых коробов.

--- Adbase/Scripts/Autocomplete.js
+++ Adbase/Scripts/Autocomplete.js
@@ -1,76 +1,6 @@
 ﻿//автозаполнение инпутов
 var autocompleteUrl;
-
-$(function () {
-
-    formUrl();
-    if ($("#Locality_id option:selected").text().toString() == "Минск")
-    {
-        $("#Street1").autocomplete({
-            source: autocompleteUrl,
-            minLength: 2,
-            select: function(event, ui) {
-
-            }
-        });
-        $("#street1").autocomplete({
-            source: autocompleteUrl,
-            minLength: 2,
-            select: function(event, ui) {
-
-            }
-        });
-    }
-    if ($("#Locality_id option:selected").text().toString() != "Минск" && $("#Street1").hasClass("ui-autocomplete-input"))
-    {
-        $("#Street1").autocomplete("destroy");
-        $("#street1").autocomplete("destroy");
-    }
-});
-$(function () {
-    var autocompleteUrl = '/Data/FindStreets';
-    $("#Street2").autocomplete({
-        source: autocompleteUrl,
-        minLength: 2,
-        select: function (event, ui) {
-
-        }
-    });
-    $("#street2").autocomplete({
-        source: autocompleteUrl,
-        minLength: 2,
-        select: function (event, ui) {
-
-        }
-    });
-});
-$(function () {
-    var autocompleteUrl = '/Data/FindStreets';
-    $("#FromStreet").autocomplete({
-        source: autocompleteUrl,
-        minLength: 2,
-        select: function (event, ui) {
-
-        }
-    });
-    $("#fromStreet").autocomplete({
-        source: autocompleteUrl,
-        minLength: 2,
-        select: function (event, ui) {
-
-        }
-    });
-});
-$(function () {
-    var autocompleteUrl = '/Data/FindStory';
-    $("#Story").autocomplete({
-        source: autocompleteUrl,
-        minLength: 2,
-        select: function (event, ui) {
-
-        }
-    });
-});
+var firstCall = true;
 
 function formUrl() {
     var cityName = $("#Locality_id option:selected").text().toString();
@@ -82,11 +12,12 @@ function formUrl() {
     autocompleteUrl = '/Data/FindStreets?' + "cityname=" + cityName;
 }
 
-$(document).ready(function () {
-    $("#Locality_id").change(function () {
-        var qst = $("#Locality_id option:selected").text().toString();
+function setAutocomplete()
+{
+     var qst = $("#Locality_id option:selected").text().toString();
         if (qst == "Минск")
         {
+
             formUrl();
             $("#Street1").autocomplete({
                 source: autocompleteUrl,
@@ -102,12 +33,140 @@ $(document).ready(function () {
 
                 }
             });
+             $("#street2").autocomplete({
+            source: autocompleteUrl,
+            minLength: 2,
+            select: function(event, ui) {
+
+            }
+             });
+             $("#Street2").autocomplete({
+            source: autocompleteUrl,
+            minLength: 2,
+            select: function(event, ui) {
+
+            }
+         });
+
+         $("#FromStreet").autocomplete({
+            source: autocompleteUrl,
+            minLength: 2,
+            select: function(event, ui) {
+
+            }
+        });
         }
         if ($("#Locality_id option:selected").text().toString() != "Минск" && $("#Street1").hasClass("ui-autocomplete-input"))
         {
             $("#Street1").autocomplete("destroy");
-            $("#street1").autocomplete("destroy");
         }
+        if ($("#Locality_id option:selected").text().toString() != "Минск" && $("#Street2").hasClass("ui-autocomplete-input"))
+        {
+            $("#Street2").autocomplete("destroy");
+           
+        }
+        if ($("#Locality_id option:selected").text().toString() != "Минск" && $("#FromStreet").hasClass("ui-autocomplete-input"))
+        {
+            $("#FromStreet").autocomplete("destroy");
+        }
+}
+
+$(document).ready(function()
+{
+    setAutocomplete();
+    $("#Locality_id").change(function()
+    {
+        setAutocomplete();
     });
-    
-})
+});
+
+//$(function () {
+
+//    formUrl();
+//    if ($("#Locality_id option:selected").text().toString() == "Минск")
+//    {
+//        $("#Street1").autocomplete({
+//            source: autocompleteUrl,
+//            minLength: 2,
+//            select: function(event, ui) {
+
+//            }
+//        });
+//        $("#street1").autocomplete({
+//            source: autocompleteUrl,
+//            minLength: 2,
+//            select: function(event, ui) {
+
+//            }
+//        });
+
+//         $("#street2").autocomplete({
+//            source: autocompleteUrl,
+//            minLength: 2,
+//            select: function(event, ui) {
+
+//            }
+//         });
+
+//         $("#FromStreet").autocomplete({
+//            source: autocompleteUrl,
+//            minLength: 2,
+//            select: function(event, ui) {
+
+//            }
+//        });
+
+//    }
+//    if ($("#Locality_id option:selected").text().toString() != "Минск" && $("#Street1").hasClass("ui-autocomplete-input"))
+//    {
+//        $("#Street1").autocomplete("destroy");
+//        $("#street1").autocomplete("destroy");
+//        $("#FromStreet").autocomplete("destroy");
+//        $("#Street2").autocomplete("destroy");
+//    }
+//});
+//$(function () {
+//    var autocompleteUrl = '/Data/FindStreets';
+//    $("#Street2").autocomplete({
+//        source: autocompleteUrl,
+//        minLength: 2,
+//        select: function (event, ui) {
+
+//        }
+//    });
+//    $("#street2").autocomplete({
+//        source: autocompleteUrl,
+//        minLength: 2,
+//        select: function (event, ui) {
+
+//        }
+//    });
+//});
+//$(function () {
+//    var autocompleteUrl = '/Data/FindStreets';
+//    $("#FromStreet").autocomplete({
+//        source: autocompleteUrl,
+//        minLength: 2,
+//        select: function (event, ui) {
+
+//        }
+//    });
+//    $("#fromStreet").autocomplete({
+//        source: autocompleteUrl,
+//        minLength: 2,
+//        select: function (event, ui) {
+
+//        }
+//    });
+//});
+//$(function () {
+//    var autocompleteUrl = '/Data/FindStory';
+//    $("#Story").autocomplete({
+//        source: autocompleteUrl,
+//        minLength: 2,
+//        select: function (event, ui) {
+
+//        }
+//    });
+//});
+
