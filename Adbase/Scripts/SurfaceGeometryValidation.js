9af0e4f1a98af09f344a01023a838e97ccec05a9
denis <ts-alan@mail.ru>
2016-02-29 15:21:28 +0200

    Дальше пишу по металлическому указателю: 1. опять все нужно выровнять и удалить цифры.

--- Adbase/Scripts/SurfaceGeometryValidation.js
+++ Adbase/Scripts/SurfaceGeometryValidation.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function ()
 {
-    $("#test1").removeAttr("novalidate");
-    var $validator = $("#test1").validate({fraction: true});
+    //$("#test1").removeAttr("novalidate");
+    //var $validator = $("#test1").validate({fraction: true});
 
     AddRules();
     
@@ -14,40 +14,8 @@
         AddRules();
     });
 
-    var widthInpts = $("input[name$='.Width']");
-    widthInpts.each(function (indx, element) {
-        
-        $(element).change(function (event) {
-                if ($(this).val() > 0) {
-                    
-                    $(this).parent().removeClass("has-error").addClass("has-success");
-                    $(this).parent().find("span.glyphicon").addClass("glyphicon-ok").removeClass("glyphicon-remove");
-                    
-                } else {
-                    $(this).removeClass("has-success").addClass("has-error");
-                     $(this).parent().find("span.glyphicon").removeClass("glyphicon-ok").addClass("glyphicon-remove");
-                    
-                }
-            });
 
-    });
-     var hightInpts = $("input[name$='.Height']");
-    hightInpts.each(function (indx, element) {
-        
-        $(element).change(function (event) {
-                if ($(this).val() > 0) {
-                    
-                    $(this).parent().removeClass("has-error").addClass("has-success");
-                    $(this).parent().find("span.glyphicon").addClass("glyphicon-ok").removeClass("glyphicon-remove");
-                    
-                } else {
-                    $(this).removeClass("has-success").addClass("has-error");
-                     $(this).parent().find("span.glyphicon").removeClass("glyphicon-ok").addClass("glyphicon-remove");
-                    
-                }
-            });
 
-    });
     //Блокирование кнопки Enter чтобы форма не отправлялась
     $(window).keydown(function(event) {
         if (event.keyCode == 13) {
@@ -66,7 +34,42 @@ function AddRules()
     var width = $("input[name$='.Width']");
     var space = $("input[name$='.Space']");
     var height = $("input[name$='.Height']");
-   
+    var widthInpts = $("input[name$='.Width']");
+
+    widthInpts.each(function (indx, element) {
+
+        $(element).change(function (event) {
+            if ($(this).val() > 0) {
+
+                $(this).parent().removeClass("has-error").addClass("has-success");
+                $(this).parent().find("span.glyphicon").addClass("glyphicon-ok").removeClass("glyphicon-remove");
+
+            } else {
+                $(this).parent().removeClass("has-success").addClass("has-error");
+                $(this).parent().find("span.glyphicon").removeClass("glyphicon-ok").addClass("glyphicon-remove");
+
+            }
+        });
+
+    });
+
+    var hightInpts = $("input[name$='.Height']");
+    hightInpts.each(function (indx, element) {
+
+        $(element).change(function (event) {
+            if ($(this).val() > 0) {
+
+                $(this).parent().removeClass("has-error").addClass("has-success");
+                $(this).parent().find("span.glyphicon").addClass("glyphicon-ok").removeClass("glyphicon-remove");
+
+            } else {
+                $(this).parent().removeClass("has-success").addClass("has-error");
+                $(this).parent().find("span.glyphicon").removeClass("glyphicon-ok").addClass("glyphicon-remove");
+
+            }
+        });
+
+    });
     if (width.length > 0)
     {
         var wId = "";
