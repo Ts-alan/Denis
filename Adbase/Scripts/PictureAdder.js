3053f1a4cfca959ee0ef0a924669ac556a6d20af
Pu4 <Pu4k0ff2012@gmail.com>
2016-03-03 12:47:32 +0300

    Заменил кнопки в неопознанных конструкциях.

--- Adbase/Scripts/PictureAdder.js
+++ Adbase/Scripts/PictureAdder.js
@@ -1,17 +1,67 @@
 ﻿$(document).ready(function()
 {
-    //Функция срабатывает при клике на кнопку загрузки файла
+    ////Функция срабатывает при клике на кнопку загрузки файла
+    //$("input[type='file']").click(function()
+    //{
+    //    //очистка кнопки выбора файла
+    //    var attr = $(this).attr("name");
+    //    var btnParent = $(this).parent();
+    //    this.value = null;
+
+    //    //удаление кнопки открытия модального окна
+    //    btnParent.find("button.modalPictureTrigger").remove();
+       
+       
+    //});
+
+    ////Функция срабатывает изменении значения кнопки загрузки файла
+    //$("input[type='file']").change(function()
+    //{
+    //    var attr = $(this).attr("name");
+    //    var modalImgId = "ModalPictureimg" + attr;
+    //    var modalId = "#ModalPictureDiv" + attr;
+    //    var btnParent = $(this).parent();
+    //    var markup = '<img id="' + modalImgId + '" src=""' + ' height="300" width="400" style="display: block;margin-left: auto;margin-right: auto;">';
+
+    //    //добавление в модальное окно пустого тега с картинкой
+    //    $(modalId).empty();
+    //    $(modalId).append(markup);
+       
+    //    //считывание загружаемой кратинки и запись её в тег src тега img
+    //    var fReader = new FileReader();
+    //    fReader.readAsDataURL(this.files[0]);
+    //    fReader.onloadend = function(event)
+    //    {
+    //        var imgid = "#" + modalImgId;
+    //         $(imgid).attr("src", event.target.result);
+    //    }
+
+    //    //добавление кнопки открывания модального окна
+    //    var modalButton = '<button type="button" class="modalPictureTrigger" data-toggle="modal" data-target="#ViewModalPicture' + attr + '" >Показать</button>';
+        
+    //    btnParent.append(modalButton);
+    //    var style = css($("input[name='photo2']"));
+    //    $(".modalPictureTrigger[data-target='#ViewModalPicture" + attr + "']").css(style);
+    //    $(".modalPictureTrigger[data-target='#ViewModalPicture" +  attr + "']").css({ width: '122px', height: '22px', '-webkit-box-shadow': '0 0 0px 1000px white inset' });
+    //});
+
+
+
+     //Функция срабатывает при клике на кнопку загрузки файла
     $("input[type='file']").click(function()
     {
         //очистка кнопки выбора файла
         var attr = $(this).attr("name");
         var btnParent = $(this).parent();
+        //btnParent.parent().find("label[for='" + attr + "']").text("Файл не выбран");
         this.value = null;
-
+       
+        btnParent.parent().find("label[for^='photo']").text("Файл не выбран");
+        btnParent.removeClass("btn-warning");
+        btnParent.addClass("btn-default");
         //удаление кнопки открытия модального окна
-        btnParent.find("button.modalPictureTrigger").remove();
-       
-       
+        btnParent.parent().find("button.modalPictureTrigger").remove();
+        
     });
 
     //Функция срабатывает изменении значения кнопки загрузки файла
@@ -21,29 +71,38 @@
         var modalImgId = "ModalPictureimg" + attr;
         var modalId = "#ModalPictureDiv" + attr;
         var btnParent = $(this).parent();
+        var fileName = this.value.split('\\').pop();
+        btnParent.parent().find("label[for^='photo']").text(fileName);
+        //Изменение стиля кнопки загрузки фала при выборе загружаемого файла
+        
         var markup = '<img id="' + modalImgId + '" src=""' + ' height="300" width="400" style="display: block;margin-left: auto;margin-right: auto;">';
-
         //добавление в модальное окно пустого тега с картинкой
+        
         $(modalId).empty();
         $(modalId).append(markup);
        
+        btnParent.addClass("btn-warning");
+        btnParent.removeClass("btn-default");
+
         //считывание загружаемой кратинки и запись её в тег src тега img
         var fReader = new FileReader();
         fReader.readAsDataURL(this.files[0]);
         fReader.onloadend = function(event)
         {
-            var imgid = "#" + modalImgId;
-             $(imgid).attr("src", event.target.result);
+           var imgid = "#" + modalImgId;
+           $(imgid).attr("src", event.target.result);
         }
 
         //добавление кнопки открывания модального окна
-        var modalButton = '<button type="button" class="modalPictureTrigger" data-toggle="modal" data-target="#ViewModalPicture' + attr + '" >Показать</button>';
+        var modalButton = '<button type="button" class="modalPictureTrigger btn btn-default" data-toggle="modal" data-target="#ViewModalPicture' + attr + '" >Показать</button>';
         
-        btnParent.append(modalButton);
-        var style = css($("input[name='photo2']"));
-        $(".modalPictureTrigger[data-target='#ViewModalPicture" + attr + "']").css(style);
-        $(".modalPictureTrigger[data-target='#ViewModalPicture" +  attr + "']").css({ width: '122px', height: '22px', '-webkit-box-shadow': '0 0 0px 1000px white inset' });
+        btnParent.parent().append(modalButton);
     });
+
+
+
+
+
 });
 
 //добавление сохраненного в проекте изображения в модальное окно для просмотра
