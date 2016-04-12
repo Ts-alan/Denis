42bcf4d2c305f105694409fa2d21a5fce584a448
Pu4 <Pu4k0ff2012@gmail.com>
2016-04-12 12:47:35 +0300

    Добавил шапки в дейтпикеры, заставил их работать в фаерфоксе, поправил скрыание полей в конструкциях с несколькими поверхностями.

--- Adbase/Scripts/DateTimeFixer.js
+++ Adbase/Scripts/DateTimeFixer.js
@@ -3,7 +3,10 @@
         // If not native HTML5 support, fallback to jQuery datePicker
             $('input[type=date]').datepicker({
                 // Consistent format with the HTML5 picker
-                    dateFormat : 'yy-mm-dd'
+                dateFormat: 'yy-mm-dd',
+                changeMonth: true,
+                changeYear: true,
+                showAnim: 'blind'
                 },
                 // Localization
                 $.datepicker.regional['it']
