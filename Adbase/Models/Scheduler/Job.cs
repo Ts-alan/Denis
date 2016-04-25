aa57df1e561860fa1fa113424aef93666160e39e
denis <ts-alan@mail.ru>
2016-04-25 10:37:49 +0200

    fix commit

---
+++ Adbase/Models/Scheduler/Job.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+
+namespace Sciencecom.Models.Scheduler
+{
+    public class Job : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            using (SciencecomEntities _context = new SciencecomEntities())
+            {
+                for (int i=0;i< _context.Surfaces.Count();i++)
+                {
+                    if (_context.Surfaces.ElementAt(i).RentFrom != null &&
+                        _context.Surfaces.ElementAt(i).RentUntil != null)
+                    {
+                        if (_context.Surfaces.ElementAt(i).RentFrom < DateTime.Now &&
+                            DateTime.Now < _context.Surfaces.ElementAt(i).RentUntil)
+                        {
+                            _context.Surfaces.ElementAt(i).isFreeOrSocial = false;
+                        }
+                        if (DateTime.Now > _context.Surfaces.ElementAt(i).RentUntil)
+                        {
+                            _context.Surfaces.ElementAt(i).isFreeOrSocial = true;
+                        }
+                    }
+                }
+            } 
+    }
+    }
+}
---
+++ Adbase/Models/Scheduler/Scheduler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using Quartz.Impl;
+
+namespace Sciencecom.Models.Scheduler
+{
+    public class Scheduler
+    {
+        public static void Start()
+        {
+            IScheduler t = StdSchedulerFactory.GetDefaultScheduler();
+            t.Start();
+
+            IJobDetail job = JobBuilder.Create<Job>().Build();
+            ITrigger trigger = TriggerBuilder.Create() 
+                .WithIdentity("trigger1", "group1") 
+                .StartAt(DateBuilder.DateOf(00, 01, 0))
+                .WithSimpleSchedule(x => x 
+                    .WithIntervalInHours(24) 
+                    .RepeatForever()) 
+                .Build();
+            t.ScheduleJob(job, trigger);
+        }
+    }
+}
